83087dcd524cf7673750c2a23879eb8bd1c25ba0
Kar1m <76733865+notkar1m@users.noreply.github.com>
2021-04-13 14:45:04 +0200

    main file

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import socket, sys
+def main(site):
+        try:
+            ips = socket.gethostbyname_ex(site)
+            # ips = socket.getprotobyname(site)
+        except socket.gaierror:
+            ips=[]
+        return ips
+try:
+    site = sys.argv[1]
+    ips = main(site)
+    print(repr(''.join(ips[2])))
+except IndexError:
+    print("You Need To Specify The Website!")
